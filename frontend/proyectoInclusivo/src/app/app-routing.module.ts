aa9232419efe0cdf45fa3098e878aa37a6714dc0
German Rindsberg <rindsberg@hotmail.com>
2022-02-22 00:01:01 -0300

    Diseño interfaz de registro de usuario. HU-2

--- frontend/proyectoInclusivo/src/app/app-routing.module.ts
+++ frontend/proyectoInclusivo/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AvisoEmpleoComponent } from './aviso-empleo/aviso-empleo.component';
+import { ModalLoginComponent } from './modal-login/modal-login.component';
 import { RegistroUsuarioComponent } from './registro-usuario/registro-usuario.component';
 
 const routes: Routes = [
-  { path: 'registro', component: RegistroUsuarioComponent }
+  { path: '', component: AvisoEmpleoComponent },
+  { path: 'registro', component: RegistroUsuarioComponent },
+  { path: 'login', component: ModalLoginComponent }
 ];
 
 @NgModule({

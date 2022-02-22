d237350fb479ad098f73bad5ce034a165d836589
German Rindsberg <rindsberg@hotmail.com>
2022-02-22 16:07:19 -0300

    Primer commit H2

--- frontend/proyectoInclusivo/src/app/app.module.ts
+++ frontend/proyectoInclusivo/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PrincipalComponent,
+    LoginComponent,
+    RegistroComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

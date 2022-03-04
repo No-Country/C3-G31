b187081bdceabb5b8e2aab6e623ba9c17eedcff4
noel toyo <=>
2022-03-04 14:27:07 -0400

    [FIX] migration fixed

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,7 +1,7 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from config.db import db, BaseModelMixin
 from .profile import Profile
-
+from .curriculum import Curriculum
 class User(db.Model, BaseModelMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
@@ -9,7 +9,7 @@ class User(db.Model, BaseModelMixin):
     profile = db.relationship(Profile, back_populates="user", uselist=False)
     #empresa = db.relationship("Empresa", back_populates="user", uselist=False)
     direccion = db.relationship("Direccion", back_populates="user", uselist=False)
-    curriculum = db.relationship("Curriculum", back_populates="user", uselist=False)
+    curriculum = db.relationship(Curriculum, back_populates="user", uselist=False)
     empresa = db.relationship("Empresa", back_populates="user", uselist=False)
 
     def set_password(self, password):

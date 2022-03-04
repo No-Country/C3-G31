8dbff2db2e5696157acc01616df0de62ac37ac95
Juan <40309961+JuanGonzalez99@users.noreply.github.com>
2022-03-04 18:15:07 -0300

    Revert "Merge pull request #4 from No-Country/HU3"
    This reverts commit e8a776d7f8a7ff8d830d5df2fd9633260c35b193, reversing
    changes made to 253539e07dc8c085396bd01cec312a2ac71e879f.

--- frontend/proyectoInclusivo/src/app/app-routing.module.ts
+++ frontend/proyectoInclusivo/src/app/app-routing.module.ts
@@ -1,17 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { EmpleosComponent } from './components/empleos/empleos.component';
-import { LoginComponent } from './components/login/login.component';
-import { PrincipalComponent } from './components/principal/principal.component';
-import { RegistroComponent } from './components/registro/registro.component';
 
-const routes: Routes = [
-  { path: '', component: PrincipalComponent },
-  { path: 'registro', component: RegistroComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'empleos', component: EmpleosComponent }
-
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/proyectoInclusivo/src/app/app.module.ts
+++ frontend/proyectoInclusivo/src/app/app.module.ts
@@ -3,32 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { PrincipalComponent } from './components/principal/principal.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegistroComponent } from './components/registro/registro.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { EmpleosComponent } from './components/empleos/empleos.component';
-import { HomeComponent } from './components/home/home.component';
-import { HttpClientModule} from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    PrincipalComponent,
-    LoginComponent,
-    RegistroComponent,
-    FooterComponent,
-    EmpleosComponent,
-    HomeComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
